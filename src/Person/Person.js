5376f4b24d0421e918d10a3a9a0cab0be14c7829
Eugen <vasilenkoevgen88@gmail.com>
2023-05-04 15:23:27 +0300

    38 Passing Method References Between Components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const person = (props) =>{
-    return <p>I"m Person I'm {props.name} {props.age} years old </p>
+    return (
+    // <p click={props.click}>I'm Person I'm {props.name} {props.age} years old </p>
+    <div>
+        <p onClick={props.click}> I'm Person I'm {props.name} {props.age} years old </p>
+        <p>{props.children}</p>
+    </div>
+    )
 }
 
 export default person;

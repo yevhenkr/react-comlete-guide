3025fb6abaa42bb6f8500d9a6bbcde5d56da71d3
Eugen <vasilenkoevgen88@gmail.com>
2023-05-04 12:36:22 +0300

    Manipulation with STATE

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import Person from './Person/Person';
 
 class App extends Component {
 
-  state ={
-    person:[
-     {name: 'Max', age:'28'},
-     {name: 'Manu', age:'29'},
-     {name: 'Stephanie', age:'26'},
-    ]
+  state = {
+    person: [
+      { name: 'Max', age: '28' },
+      { name: 'Manu', age: '29' },
+      { name: 'Stephanie', age: '26' },
+    ],
+    otherState: 'some other value'
   }
 
-  swithNameHandler = () =>{
+  swithNameHandler = () => {
     // console.log('Was clicked ');
-    this.state.person[0].name = 'Maximilian';
+    //DON'T DO THIS:  this.state.person[0].name = 'Maximilian';
+
+    this.setState({
+      person: [
+        { name: 'Maximilian', age: '28' },
+        { name: 'Manu', age: '29' },
+        { name: 'Stephanie', age: '27' },
+      ]
+    } )
   }
   render() {
     return (
@@ -22,10 +31,10 @@ class App extends Component {
         <h1>Hi, I'm React App</h1>
         <p>This is realy working!</p>
         <button onClick={this.swithNameHandler}>Switch name</button>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} />
         <Person name={this.state.person[1].name} age={this.state.person[1].age} >My hobies recing</Person>
-        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
-      </div> 
+        <Person name={this.state.person[2].name} age={this.state.person[2].age} />
+      </div>
     );
     // return React.createElement('div',  {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m React App!!!' ));
 

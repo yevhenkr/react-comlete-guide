d85bf01c9d9d4ab80a0ce1e898ccd2c1dd61376d
Eugen <vasilenkoevgen88@gmail.com>
2023-05-04 12:23:50 +0300

    Handel event with metod

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ class App extends Component {
      {name: 'Stephanie', age:'26'},
     ]
   }
+
+  swithNameHandler = () =>{
+    console.log('Was clicked   ');
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p>This is realy working!</p>
-        <button>Switch name</button>
+        <button onClick={this.swithNameHandler()}>Switch name</button>
         <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
         <Person name={this.state.person[1].name} age={this.state.person[1].age} >My hobies recing</Person>
         <Person name={this.state.person[2].name} age={this.state.person[2].age}/>

8d201d873d657dfcaf1305ccc596b304d7105f65
Eugen <vasilenkoevgen88@gmail.com>
2023-05-04 12:14:40 +0300

    Added State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state ={
+    person:[
+     {name: 'Max', age:'28'},
+     {name: 'Manu', age:'29'},
+     {name: 'Stephanie', age:'26'},
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
-        <p>Work</p>
+        <p>This is realy working!</p>
+        <button>Switch name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age} >My hobies recing</Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
       </div> 
     );
     // return React.createElement('div',  {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m React App!!!' ));
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = (props) =>{
+    return <p>I"m Person I'm {props.name} {props.age} years old </p>
+}
+
+export default person;

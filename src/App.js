eda9992f0020ca4737a6bc9abc743ba0f436799b
Eugen <vasilenkoevgen88@gmail.com>
2023-05-05 13:42:18 +0300

    50 Lists  Keys
    add keys to list component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: '28' },
-      { name: 'Manu', age: '29' },
-      { name: 'Stephanie', age: '26' }
+      { id: 'asfa1', name: 'Max', age: '28' },
+      { id: 'vasdf1', name: 'Manu', age: '29' },
+      { id: 'asdf11', name: 'Stephanie', age: '26' }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -17,8 +17,8 @@ class App extends Component {
   nameChangehandler = (event) => {
     this.setState({
       person: [
-        { name: 'Max', age: '28' },
-        { name: event.target.value, age: '29' },
+        {  name: 'Max', age: '28' },
+        {  name: event.target.value, age: '29' },
         { name: 'Stephanie', age: '26' }
       ]
     })
@@ -52,7 +52,8 @@ class App extends Component {
             return <Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
-              age={person.age} />
+              age={person.age}
+              key={person.id} />
           })}
         </div>
       );

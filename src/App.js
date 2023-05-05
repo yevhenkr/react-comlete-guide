dc41262a6bf4a13529b0784435fdd9600e3d272f
Eugen <vasilenkoevgen88@gmail.com>
2023-05-05 15:23:26 +0300

    53 (2-3)Create a new component ( ValidationComponent
    which receives the text length as a prop</li>
    - Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Validation from './Vaildation/Vaildation';
 
 class App extends Component {
 
@@ -31,6 +32,7 @@ class App extends Component {
         onChange={this.inputChangedHandler} 
         value={this.state.userInput}/>
         <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length}/>
       </div>
     );
   }

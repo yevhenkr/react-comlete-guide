caa2403ecdcff68643bcf5f57c28881e99c56a7a
Eugen <vasilenkoevgen88@gmail.com>
2023-05-05 10:31:25 +0300

    Added Otput Input

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component{
     username: 'supermax'
   }
 
-  userChangebleHandler = event =>{
-    this.setState({username: event.target.value})
+  userChangebleHandler = (event) =>{
+    this.setState({username: event.target.value});
   }
 
   render(){
@@ -20,17 +20,16 @@ class App extends Component{
           <li>UserInput should hold an input element, UserOutput two paragraphs</li>
           <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
           <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
-          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
-          <li>Add a method to manipulate the state (=> an event-handler method)</li>
+          <li>Add state to the App component (=* the username) and pass the username to the UserOutput component</li>
+          <li>Add a method to manipulate the state (=* an event-handler method)</li>
           <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
           <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
           <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
           <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
         </ol>
         <UserInput 
-          changed={this.usernameChangedHandler} 
-          currentName={this.state.username} />
-        <UserOutput userName={this.state.username} />
+          changed={this.userChangebleHandler}
+          currentName={this.state.username}  />
         <UserOutput userName={this.state.username} />
         <UserOutput userName="Max" />
       </div>
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const userOutput = (props) => {
+    return (
+        <div>
+            <p>UserName: {props.userName}</p>
+            <p>I hope I'll be overrwritting!</p>
+        </div>
+    );
+};
+
+export default userOutput;

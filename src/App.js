425c594bb87c0a917301ae238fdbdc70fb535473
Eugen <vasilenkoevgen88@gmail.com>
2023-05-08 19:23:01 +0300

    63 Working with CSS Modules
    модули css. Плюс  втом что файлі можно сепарировать. И видить уникальніе названия классов в окне раработчика
    npm run eject
    получим файл 2 файла конфиг config
    test: /\.css$/,
                use: [
                  require.resolve('style-loader'),
                  {
                    loader: require.resolve('css-loader'),
                    options: {
                      importLoaders: 1,
                      modules: true,////////////////////////////добавить в два фафйла
                      localIdentName: '[name]__[local]__[hash:base64:5]'//////////////////////добавить в два фафйла
                    },
                  },

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -43,16 +43,9 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   }
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
+
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -68,26 +61,23 @@ class App extends Component {
           })}
         </div>
       );
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: 'black'
-      // }
+      
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm React App</h1>
-        <p className={classes.join(' ')}>This is realy working!</p>
-        <button className='button'
+        <p className={assignedClasses.join(' ')}>This is realy working!</p>
+        <button className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>

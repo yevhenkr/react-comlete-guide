17d5bc9577d2297d805e56d527c70c6907de8d21
Eugen <vasilenkoevgen88@gmail.com>
2023-05-08 12:11:21 +0300

    58 Added style class dynamycli

--- src/App.js
+++ src/App.js
@@ -71,10 +71,18 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
-        <p>This is realy working!</p>
+        <p className={classes.join(' ')}>This is realy working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>

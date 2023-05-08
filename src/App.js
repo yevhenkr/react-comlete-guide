d7c718729048bfd94b9cf26f26e5ff041c4a5345
Eugen <vasilenkoevgen88@gmail.com>
2023-05-08 15:25:04 +0300

    61 Use styled components for add teg and style button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button` 
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue,;
+  padding: 8px;
+  cursor: pointer;
+ 
+  &:hover {
+    background-color: lightgreen;
+    color: black;
+  }`
+  ;
 
 
 class App extends Component {
@@ -62,8 +77,8 @@ class App extends Component {
               name={person.name}
               age={person.age}
               key={person.id}
-              changed={(event) => this.nameChangeHandler(event, person.id)} 
-              //changed: функция-обработчик события onChange, которая вызывается при изменении значения поля ввода, передавая в нее объект события и уникальный идентификатор человека.
+              changed={(event) => this.nameChangeHandler(event, person.id)}
+            //changed: функция-обработчик события onChange, которая вызывается при изменении значения поля ввода, передавая в нее объект события и уникальный идентификатор человека.
             />
           })}
         </div>
@@ -83,10 +98,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p className={classes.join(' ')}>This is realy working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-
+        <StyledButton
+          onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
         {persons}
       </div>
     );

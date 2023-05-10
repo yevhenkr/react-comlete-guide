0755320d10ccbfb54b299b3b6ab011af27724aab
Eugen <vasilenkoevgen88@gmail.com>
2023-05-10 11:09:25 +0300

    Pure component

--- src/Сomponents/Persons/Persons.js
+++ src/Сomponents/Persons/Persons.js
@@ -1,17 +1,20 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
 
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('[Persons.js] shouldComponentUpdate');
-        if (nextProps.person !== this.props.persons) {
-            return true;
-        } else {
-            return false;
-        }
-        // return true;
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log('[Persons.js] shouldComponentUpdate');
+    //     if (nextProps.person !== this.props.persons || 
+    //         nextProps.changed !== this.props.changed  || 
+    //         nextProps.clicked !== this.props.clicked
+    //         ) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    //     // return true;
+    // }
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
         console.log('[Persons.js] getSnapshotBeforeUpdate');

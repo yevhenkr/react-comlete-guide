31eebd6a40e9ca7b2d41a8a3ba6d3d017fc255e7
Eugen <vasilenkoevgen88@gmail.com>
2023-05-11 15:15:11 +0300

    Revert "130 add and delrte folder and file"
    This reverts commit 6917ee4c7eb5c4c9843ddc3fc918e5f16e9eed68.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
+import Person from './Person/Person';
+
 
 class App extends Component {
+  state = {
+    persons: [
+      { id: 'asfa1', name: 'Max', age: '28' },
+      { id: 'vasdf1', name: 'Manu', age: '29' },
+      { id: 'asdf11', name: 'Stephanie', age: '26' }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  };
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id; //true, если условие выполнено то возвращает index
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    // const person =Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  }
   render() {
+    let persons = null;
+    let btnClass = '';
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangeHandler(event, person.id)}
+            //changed: функция-обработчик события onChange, которая вызывается при изменении значения поля ввода, передавая в нее объект события и уникальный идентификатор человека.
+            />
+          })}
+        </div>
+      );
+      
+      btnClass = classes.Red;
+    }
+
+    const assignedClasses = [];
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push(classes.red);
+    }
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold);
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={classes.App}>
+        <h1>Hi, I'm React App</h1>
+        <p className={assignedClasses.join(' ')}>This is realy working!</p>
+        <button className={btnClass}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
+    // return React.createElement('div',  {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m React App!!!' ));
   }
 }
+
 
 export default App;

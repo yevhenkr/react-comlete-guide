f7ab953a739c1c97c6c58b36ae3d4e6cf2d5b1ce
Eugen <vasilenkoevgen88@gmail.com>
2023-05-12 09:39:02 +0300

    131 Added Layout and Aux

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Aux from '../../hoc/Auxilary';
+
+const layout = (props) => (
+<Aux>
+    <div>Toolba, SideDrawer, Backdrop</div>
+    <main>
+        {props.children}
+    </main>
+</Aux>
+)
+
+export default layout;

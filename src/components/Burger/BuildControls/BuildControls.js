25538d53d09399f3e6e79e3af5f1e34b5332a4d0
Eugen <vasilenkoevgen88@gmail.com>
2023-05-12 16:48:47 +0300

    Logic show and aded picec of burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -3,17 +3,21 @@ import React from 'react';
 import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
-const controls =[
-    {label: 'Salade', type: 'salad'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'cheese'},
-    {label: 'Meat', type: 'meat'},
+const controls = [
+    { label: 'Salade', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
 ];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        {controls.map(ctrl =>(
-            <BuildControl key={ctrl.label} label ={ctrl.label}/>
+        {controls.map(ctrl => (
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+            />
         ))}
     </div>
 )

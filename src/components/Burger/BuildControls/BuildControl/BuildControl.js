c91c826ffdf294f980446fca42e80de2b4c61b40
Eugen <vasilenkoevgen88@gmail.com>
2023-05-13 11:54:30 +0300

    141 Disbled button when youy can't Deduction

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -8,6 +8,7 @@ const buildControl = (props) => (
         >{props.label}</div>
         <button className={classes.Less}
         onClick={props.removed}
+            disabled={props.disabled}
         >Less</button>
         <button className={classes.More} onClick={props.added}
         >More</button>

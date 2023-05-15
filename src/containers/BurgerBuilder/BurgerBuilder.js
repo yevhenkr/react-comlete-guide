98934b546d54177243dc66f529e0c22be790abc9
Eugen <vasilenkoevgen88@gmail.com>
2023-05-15 10:49:28 +0300

    144 Creating the Order Summary Modal
    Создали табличку на которой в реальном времени отбражаеться данніе  state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Aux from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -76,14 +78,20 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Burger ingredients={this.state.ingredients} />
-        <BuildControls
-          ingredientAdded={this.addIngredientHandler}
-          ingredientRemoved={this.removeIngredientHandler}
-          disabled={disabledInfo}
-          purchaseble={this.state.purchaseble}
-          price={this.state.totalPrice}
-        />
+        <Modal>
+          <OrderSummary 
+          ingredients={this.state.ingredients}
+          />
+        </Modal>
+
+          <Burger ingredients={this.state.ingredients} />
+          <BuildControls
+            ingredientAdded={this.addIngredientHandler}
+            ingredientRemoved={this.removeIngredientHandler}
+            disabled={disabledInfo}
+            purchaseble={this.state.purchaseble}
+            price={this.state.totalPrice}
+          />
       </Aux>
     );
   }

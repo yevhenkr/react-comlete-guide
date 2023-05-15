7e90d414c4bfeff7fd1d3273bcd11191e559f1b2
Eugen <vasilenkoevgen88@gmail.com>
2023-05-15 15:09:13 +0300

    153 Creating a Responsive Sidedrawer 154 Working on Responsive Adjustments

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from "react";
 import Aux from '../../hoc/Auxilary';
 import classes from './Layout.css';
 import Toolbar  from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <div>Toolba, SideDrawer, Backdrop</div>
         <main className={classes.Content}
         >
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
-<div className={classes.Logo}>
+<div className={classes.Logo} >
     <img src={burgerLogo} alt='MyBurger'/>
 </div>
 );
